3344fcbf9ab1e97d37287a859f704b2735c4710b
SamHeutmaker <samheutmaker@gmail.com>
2018-11-15 21:27:09 -0700

    fix testing;

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import './styles/App.scss';
 import DialogueScreen from './components/DialogueScreen';
 import StudyTrial from './components/StudyTrial';
+import oneRandom from './util/oneRandom';
 import {
   learningGroup1,
   learningGroup2,
@@ -12,7 +13,6 @@ import {
   gameBeans,
   testBeans,
 } from './util/BeanGroups';
-
 
 class App extends Component {
   constructor(){
@@ -25,24 +25,26 @@ class App extends Component {
   stages = [
     <DialogueScreen key={1} dialogue="1" goToNextScreen={this.goToNextScreen.bind(this)} />, // Introduction
     <DialogueScreen key={2} dialogue="2" goToNextScreen={this.goToNextScreen.bind(this)} />, // Instructions
-    <StudyTrial key={3} beans={learningGroup1} handleResponse={this.handleResponse.bind(this)} />, // test
-    <StudyTrial key={4} beans={learningGroup2} handleResponse={this.handleResponse.bind(this)} />, // test
-    <StudyTrial key={5} beans={learningGroup3} handleResponse={this.handleResponse.bind(this)} />, // test
-    <StudyTrial key={6} beans={learningGroup4} handleResponse={this.handleResponse.bind(this)} />, // test
-    <StudyTrial key={7} beans={learningGroup5} handleResponse={this.handleResponse.bind(this)} />, // test
-    <StudyTrial key={8} beans={learningGroup6} handleResponse={this.handleResponse.bind(this)} />, // test
-    <DialogueScreen key={9} dialogue="3" goToNextScreen={this.goToNextScreen.bind(this)} />, // pracitce blocks complete
-    <DialogueScreen key={10} dialogue="4" goToNextScreen={this.goToNextScreen.bind(this)} />, // learning phase 1
-    <StudyTrial key={11} beans={gameBeans} handleResponse={this.handleResponse.bind(this)} />, // learning block 1
-    <DialogueScreen key={12} dialogue="5" goToNextScreen={this.goToNextScreen.bind(this)} />, // first learning block complete -- take a break
-    <DialogueScreen key={13} dialogue="6" goToNextScreen={this.goToNextScreen.bind(this)} />, // second learning block - continue where left off
-    <StudyTrial key={14} beans={gameBeans} handleResponse={this.handleResponse.bind(this)} />, // learning block 2
-    <DialogueScreen key={15} dialogue="7" goToNextScreen={this.goToNextScreen.bind(this)} />, // second block complete -- take a break
-    <DialogueScreen key={16} dialogue="8" goToNextScreen={this.goToNextScreen.bind(this)} />, // third block start -- continue where left off
-    <StudyTrial key={17} beans={gameBeans} handleResponse={this.handleResponse.bind(this)} />, // learning block 3
-    <DialogueScreen key={18} dialogue="9" goToNextScreen={this.goToNextScreen.bind(this)} />, // Final phase -- actual test
-    <StudyTrial key={19} beans={testBeans} handleResponse={this.handleResponse.bind(this)} />, // this is the test!
-    <DialogueScreen key={20} dialogue="10" goToNextScreen={this.goToNextScreen.bind(this)} /> // Goodbyee!
+    <StudyTrial key={3} beans={[
+      oneRandom(learningGroup1),
+      oneRandom(learningGroup2),
+      oneRandom(learningGroup3),
+      oneRandom(learningGroup4),
+      oneRandom(learningGroup5),
+      oneRandom(learningGroup6),
+    ]} handleResponse={this.handleResponse.bind(this)} />, // test
+    <DialogueScreen key={4} dialogue="3" goToNextScreen={this.goToNextScreen.bind(this)} />, // pracitce blocks complete
+    <DialogueScreen key={5} dialogue="4" goToNextScreen={this.goToNextScreen.bind(this)} />, // learning phase 1
+    <StudyTrial key={6} beans={gameBeans} handleResponse={this.handleResponse.bind(this)} />, // learning block 1
+    <DialogueScreen key={7} dialogue="5" goToNextScreen={this.goToNextScreen.bind(this)} />, // first learning block complete -- take a break
+    <DialogueScreen key={8} dialogue="6" goToNextScreen={this.goToNextScreen.bind(this)} />, // second learning block - continue where left off
+    <StudyTrial key={9} beans={gameBeans} handleResponse={this.handleResponse.bind(this)} />, // learning block 2
+    <DialogueScreen key={10} dialogue="7" goToNextScreen={this.goToNextScreen.bind(this)} />, // second block complete -- take a break
+    <DialogueScreen key={11} dialogue="8" goToNextScreen={this.goToNextScreen.bind(this)} />, // third block start -- continue where left off
+    <StudyTrial key={12} beans={gameBeans} handleResponse={this.handleResponse.bind(this)} />, // learning block 3
+    <DialogueScreen key={13} dialogue="9" goToNextScreen={this.goToNextScreen.bind(this)} />, // Final phase -- actual test
+    <StudyTrial key={14} beans={testBeans} handleResponse={this.handleResponse.bind(this)} />, // this is the test!
+    <DialogueScreen key={15} dialogue="10" goToNextScreen={this.goToNextScreen.bind(this)} /> // Goodbyee!
   ];
 
   advanceToNextStage(){
--- src/util/oneRandom.js
+++ src/util/oneRandom.js
@@ -1,3 +1,10 @@
 export default function oneRandom(array) {
-  
+  let random = getRandomInt(0, array.length -1 );
+  return array[random];
+}
+
+function getRandomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
 }
